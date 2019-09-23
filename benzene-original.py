1bd424c471832dd41021c5be123556a0378f2f03
ved1717 <47635634+ved1717@users.noreply.github.com>
2019-09-23 21:33:37 +0530

    Add files via upload

---
+++ benzene-original.py
@@ -0,0 +1,9 @@
+import turtle
+colors =['red','yellow','green','purple','blue','orange']
+t =turtle.Pen() 
+turtle.bgcolor('black')
+for x in range(1000):
+ t.pencolor(colors[x%6])
+ t.width(x/100+1)
+ t.forward(x)
+ t.left(59)
